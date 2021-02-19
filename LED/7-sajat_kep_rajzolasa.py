d796cb6cad57276f1445e189ff20ef0cb648904d
nagyfrantisek <79305148+nagyfrantisek@users.noreply.github.com>
2021-02-19 09:45:06 +0100

    Create 7-sajat_kep_rajzolasa.py

---
+++ LED/7-sajat_kep_rajzolasa.py
@@ -0,0 +1,8 @@
+from microbit import *
+
+# rakéta megrajzolás pixelenként/ledenként
+# 0-9 LED erőssége
+raketa = Image("00900:09990:09590:09990:90909:")
+
+# rakéta kép megjelenítése
+display.show(raketa)
