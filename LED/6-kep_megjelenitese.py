faf275177183ec14d7905eacc3d7801b46d8c1f1
nagyfrantisek <79305148+nagyfrantisek@users.noreply.github.com>
2021-02-19 09:41:08 +0100

    Create 6-kep_megjelenitese.py

---
+++ LED/6-kep_megjelenitese.py
@@ -0,0 +1,16 @@
+from microbit import *
+
+#szomoru - kep megjelenitese
+display.show(Image.SAD)
+sleep(1000)
+
+#mosoly - kep megjelenitese
+display.show(Image.SMILE)
+sleep(1000)
+
+#boldog - kep megjelenitese
+display.show(Image.HAPPY)
+sleep(1000)
+
+#LED kijelző törlése
+display.clear()
